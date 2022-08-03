63f376ac08df8f348eb47ec7038403924816728b
ささき <castanie96@gmail.com>
2022-08-03 23:30:39 +0900

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,23 @@
+import { useEffect } from 'react'
 import type { NextPage } from 'next'
+import crypto from 'crypto'
+import { useRouter } from 'next/router'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
 export const getStaticProps = async() => {
-  const time = new Date()
+  const time = new Date(Date.now() + ((new Date().getTimezoneOffset() + (9 * 60)) * 60 * 1000))
+  const base_time = new Date(time)
+  const result_time = (base_time.getMonth() + 1) + '/' + base_time.getDate() + ' ' + base_time.getHours() + ':' + base_time.getMinutes() + ':' + base_time.getSeconds()
+
+  const cipher = crypto.createCipheriv("aes-256-cbc", process.env.NEXT_PUBLIC_KEY as string, crypto.randomBytes(16))
+  const crypted = cipher.update(result_time, 'utf-8', 'hex')
+  const crypted_text = crypted + cipher.final('hex')
 
   return {
     props: {
-      time: String(time)
+      time: String(result_time),
+      ct: crypted_text
     },
     revalidate: 15,
   }
@@ -15,20 +25,29 @@ export const getStaticProps = async() => {
 
 type Props = {
   time: string
+  ct: string
 }
 
-const Home: NextPage<Props> = ({ time }) => {
-  const base_time = new Date(time)
-  const result_time = (base_time.getMonth() + 1) + '/' + base_time.getDate() + ' ' + base_time.getHours() + ':' + base_time.getMinutes() + ':' + base_time.getSeconds()
+const Home: NextPage<Props> = ({ time, ct }) => {
+  const router = useRouter()
+
+  useEffect(() =>{
+    router.push({
+      pathname: router.pathname,
+      query: { ct }
+    }, undefined, {
+      shallow: true
+    });
+  }, []);
 
   return (
     <div className={styles.container}>
       <Head>
-        <title>{ result_time }</title>
+        <title>{ time }</title>
         <meta property="og:url" content="/" />
-        <meta property="og:image" content={ `https://og-image-rust-eta.vercel.app/${ result_time }.png?test=aaa` } />
+        <meta property="og:image" content={ `https://og-image-rust-eta.vercel.app/${ time }.png` } />
         <meta property="og:title" content="ページのタイトル" />
-        <meta property="og:description" content={ result_time } />
+        <meta property="og:description" content={ time } />
 
         <meta name="twitter:card" content="summary_large_image" />
 
@@ -37,8 +56,8 @@ const Home: NextPage<Props> = ({ time }) => {
           <meta property='og:image:height' content='630' />
       </Head>
 
-      <h3 className={ styles.title }>{ result_time }</h3>
-      <img className={ styles.image } src={ `https://og-image-rust-eta.vercel.app/${ result_time }.png?test=aaa` } alt='現在自国' />
+      <h3 className={ styles.title }>{ time }</h3>
+      <img className={ styles.image } src={ `https://og-image-rust-eta.vercel.app/${ time }.png` } alt='現在自国' />
     </div>
   )
 }
